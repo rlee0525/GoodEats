537fa8b547e1566d7916a40a1b799b4f7d752f66
Raymond Lee <raylee0525@gmail.com>
2017-10-31 22:00:40 -0700

    configure google maps / autocomplete / home page / search page

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -6,6 +6,11 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-container">
+        <img
+          id="logo"
+          src="http://res.cloudinary.com/rlee0525/image/upload/v1509507417/logo_iv3h6z.png"
+        />
+        <div className="instructions">Only the best restaurants near you</div>
         <Search />
       </div>
     );
--- src/modules/search/index.js
+++ src/modules/search/index.js
@@ -2,20 +2,63 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { queryPlaces } from './actions';
+import Map from '../map';
+
+const google = window.google;
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      query: ""
+      query: "",
+      center: { lat: 37.783052, lng: -122.39103 },
+      zoom: 14,
+      address: ""
     };
+
+    this.updateAddress = this.updateAddress.bind(this);
+  }
+
+  componentDidMount() {
+    let input = document.getElementById('address');
+    let autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', () => {
+      let place = autocomplete.getPlace();
+      let address = place.formatted_address;
+      let lat = place.geometry.location.lat();
+      let lng = place.geometry.location.lng();
+
+      this.setState({
+        address,
+        lat,
+        lng
+      });
+    });
+  }
+
+  updateAddress() {
+    let address = document.getElementById('address').value;
+    this.setState({ address });
   }
 
   render() {
     return (
       <div className="search-container">
-        Hello
+        <div className="search-bar">
+          <input
+            maxLength={50}
+            value={this.state.address}
+            onChange={this.updateAddress}
+            type="text"
+            id="address"
+            placeholder="Where are you?"
+            autoFocus={true}
+          />
+        </div>
+
+        {/* <Map center={this.state.center} address={this.state.address} /> */}
       </div>
     );
   }
